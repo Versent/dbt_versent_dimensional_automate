2abc40eae2cf968bc947c9de7eaf29b4c817e51b
Tim Napier <timothy.napier@versent.com.au>
2025-02-12 04:31:50 +0000

    First cut cdc columns

--- macros/support/change_data_capture.sql
+++ macros/support/change_data_capture.sql
@@ -1,23 +1,12 @@
 {%- macro type2_columns(
+    business_key,
     type2
     ) -%}
-        -- type2 columns
-                {%- set as_of_date_source = type2.as_of_date %}
-                {{ as_of_date_source }}     as as_of_date,
-                {{ early_date() }}          as early_date,
-                {{ late_date() }}           as late_date,
-                lead({{ as_of_date_source }} ) 
-                    over (
-                        partition by {{business_key}}  
-                        order by {{ as_of_date_source }} 
-                    ) 
-                    -- {{ target.type }}
-                        {%- if target.type == "databricks" -%}
-                        - INTERVAL 1 microsecond     
-                        {%- elif target.type == "snowflake" -%}          
-                        - INTERVAL '1 MICROSECONDS'
-                        {% endif %}
-                                            as next_date,
+    -- type2 columns
+            {%- set as_of_date_source = type2.as_of_date %}
+            {{ effective_from_datetime(business_key, as_of_date_source)}}  as effective_from_datetime,
+            {{ effective_to_datetime(business_key, as_of_date_source)}}  as effective_to_datetime,
+            {{ is_current(business_key, as_of_date_source)}}  as is_current,
 
 {%- endmacro %}
 {%- macro early_date() -%}
@@ -25,4 +14,77 @@
 {%- endmacro %}
 {%- macro late_date() -%}
     cast('2999-01-01 00:00:01'   as timestamp) as late_date,  
- {%- endmacro %}  
+{%- endmacro %}  
+{%- macro previous_date( 
+    business_key,
+    as_of_date_source
+    ) -%}
+    lag(as_of_date) 
+            over (
+            partition by {{business_key}}   
+            order by {{ as_of_date_source }} 
+{%- endmacro -%}
+{%- macro next_date( 
+    business_key,
+    as_of_date_source
+    )  
+    -%}
+    lead({{ as_of_date_source }} ) 
+    over (
+        partition by {{business_key}}  
+        order by {{ as_of_date_source }} 
+    ) 
+    -- {{ target.type }}
+        {%- if target.type == "databricks" -%}
+        - INTERVAL 1 microsecond     
+        {%- elif target.type == "snowflake" -%}          
+        - INTERVAL '1 MICROSECONDS'
+        {% endif %}
+{%- endmacro %}  
+{%- macro effective_from_datetime(
+    business_key,
+    as_of_date_source
+    ) -%}
+    iff(
+    -- previous_date
+        {{ previous_date(business_key, as_of_date)}}
+    is null, 
+    {{ early_date() }}, 
+    as_of_date) as effective_from_datetime,
+ -%}
+
+{%- endmacro %}  
+{%- macro effective_to_datetime(
+    business_key,
+    as_of_date_source
+    ) -%}
+    coalesce(
+        -- next_date - 1 very small bit
+            {{ next_date(business_key_name, as_of_date)}} 
+            -- {{ target.type }}
+            {% if target.type == "databricks" %}
+            - INTERVAL 1 microsecond     
+            {% elif target.type == "snowflake" %}          
+            - INTERVAL '1 MICROSECONDS'
+            {% endif %}, 
+        late_date
+        )
+
+{%- endmacro%}
+{%- macro is_current(
+    business_key,
+    as_of_date_source
+    ) -%}
+    iff(
+        -- next_date
+        (
+            lead(as_of_date) 
+                over (
+                partition by customer_id  
+                order by as_of_date
+                ) 
+        )is null,
+        true,
+        false
+    )    
+{%- endmacro%}                               
